669768eb86894faea5070e42d6130237b79b1f79
RikiPlatas <rikiplatas11@gmail.com>
2023-07-10 13:25:48 +0200

    Creacion proyecto
    Se creo el proyecto con spring initializr y se desplego la rama en remoto

---
+++ testRestFul/src/main/java/com/dex/testRestFul/TestRestFulApplication.java
@@ -0,0 +1,13 @@
+package com.dex.testRestFul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestFulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRestFulApplication.class, args);
+	}
+
+}
---
+++ testRestFul/src/test/java/com/dex/testRestFul/TestRestFulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dex.testRestFul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRestFulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
